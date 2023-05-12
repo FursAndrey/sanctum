b5b84ccaa32a616b45db46ba41b4f2423f769d4a
Furs Andrei <you@example.com>
2023-05-12 18:53:24 +0300

    add post model migration controller requests resource

---
+++ app/Http/Controllers/Admin/PostController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Post\StoreRequest;
+use App\Http\Requests\Post\UpdateRequest;
+use App\Http\Resources\PostResource;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $posts = Post::latest()->get();
+        
+        return PostResource::collection($posts);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreRequest $request)
+    {
+        $data = $request->validated();
+        $post = Post::create($data);
+        
+        return new PostResource($post);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Post $post)
+    {
+        return new PostResource($post);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateRequest $request, Post $post)
+    {
+        $post->fill($request->validated())->save();
+        
+        return new PostResource($post);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Post $post)
+    {
+        $post->delete();
+
+        return response()->noContent();
+    }
+}
