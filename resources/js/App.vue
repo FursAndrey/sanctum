29a59bb85440edc97d1853fc358cafc68e245c64
Furs Andrei <you@example.com>
2023-05-14 10:44:08 +0300

    add logout btn

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div class="admin-menu">
+        <div v-if="token" class="admin-menu">
             <div class="flex justify-between w-96 mx-auto">
                 <router-link :to="{ name: 'post.index'}" class="p-2">Posts</router-link>
             </div>
@@ -9,8 +9,9 @@
             <div class="flex justify-between w-96 mx-auto">
                 <router-link :to="{ name: 'Index'}" class="p-2">Index</router-link>
                 <router-link :to="{ name: 'Page'}" class="p-2">Page</router-link>
-                <router-link :to="{ name: 'login'}" class="p-2">Login</router-link>
-                <router-link :to="{ name: 'registration'}" class="p-2">Registration</router-link>
+                <router-link v-if="!token" :to="{ name: 'login'}" class="p-2">Login</router-link>
+                <router-link v-if="!token" :to="{ name: 'registration'}" class="p-2">Registration</router-link>
+                <span v-if="token" @click.prevent="logout" class="p-2 cursor-pointer">Logout</span>
             </div>
         </div>
         <h1 class="text-3xl font-bold underline">
@@ -25,8 +26,34 @@
 export default {
     name: "App",
     data() {
-        
+        return {
+            token: null
+        }
     },
+    
+    mounted() {
+        this.getToken()
+    },
+
+    watch: {
+        $route(to, from) {
+            this.getToken()
+        }
+    },
+
+    methods: {
+        getToken() {
+            this.token = localStorage.getItem('x_xsrf_token')
+        },
+
+        logout() {
+            axios.post('/logout')
+                .then( res => {
+                    localStorage.removeItem('x_xsrf_token')
+                    this.$router.push({name: 'Index'})
+                })
+        }
+    }
 }
 </script>
 
