debc14b9c0a4a3c3121517679b6a26565a4c0baf
Furs Andrei <you@example.com>
2023-05-27 18:03:07 +0300

    add post and role seeders

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('roles')->insert(
+            [
+                ['title'=>'Admin', 'discription'=>'Creator of this site', 'created_at'=>Carbon::now(), 'updated_at'=>Carbon::now()],
+            ]
+        );
+    }
+}
