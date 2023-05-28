7505e6555f2bbee2dbe9336cd82dc4750fcbbe2d
Furs Andrei <you@example.com>
2023-05-28 17:19:17 +0300

    change description for project

--- resources/js/views/about.vue
+++ resources/js/views/about.vue
@@ -1,9 +1,30 @@
 <template>
     <div>
-        <h1 class="text-3xl font-bold text-center">About the project</h1>
-        <p class="mt-4">
+        <h1 class="text-3xl font-bold text-center mb-4">About the project</h1>
+        <p>
             Это учебный проект. Он создан исключительно для изучения веб-разработки. Весь контент подготовлен при помощи генераторов случайных тектов и изображений.
         </p>
+        <h3 class="text-xl font-bold text-center my-3">Описание функционала проекта</h3>
+        <ol class="list-disc">
+            <li>
+                <span class="font-bold">Для не авторизованных пользователей и пользователей, не иеющих роли "администратор", доступны:</span>
+                <ol class="list-decimal">
+                    <li>форма для авторизации</li>
+                    <li>форма для регистрации</li>
+                    <li>просмотр списка постов</li>
+                    <li>просмотр страницы поста</li>
+                    <li>просмотр статичной страницы</li>
+                </ol>
+            </li>
+            <li>
+                <span class="font-bold">Для пользователей, иеющих роль "администратор", доступны:</span>
+                <ol class="list-decimal">
+                    <li>CRUD для постов</li>
+                    <li>CRUD для ролей</li>
+                    <li>удаление пользователей и назначение им ролей</li>
+                </ol>
+            </li>
+        </ol>
     </div>
 </template>
 
