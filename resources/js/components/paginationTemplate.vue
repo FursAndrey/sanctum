7cbb79d813429f6c03adff46a9e3b78b91b2dddb
Furs Andrei <you@example.com>
2023-06-03 17:29:46 +0300

    change pagination style

--- resources/js/components/paginationTemplate.vue
+++ resources/js/components/paginationTemplate.vue
@@ -2,10 +2,54 @@
     <div>
         <div v-if="meta" class="pt-4 pb-6">Posts form: {{ meta.from }} to: {{ meta.to }}. Total posts: {{ meta.total }}.</div>
         <div v-if="meta" class="pb-2 mb-8">
-            <span @click="firstPage()" class="w-16 p-2 me-4 bg-amber-600 rounded-lg text-center cursor-pointer">first</span>
-            <span @click="previusPage()" class="w-16 p-2 me-4 bg-amber-600 rounded-lg text-center cursor-pointer">previus</span>
-            <span @click="nextPage()" class="w-16 p-2 me-4 bg-amber-600 rounded-lg text-center cursor-pointer">next</span>
-            <span @click="lastPage()" class="w-16 p-2 bg-amber-600 rounded-lg text-center cursor-pointer">last</span>
+            <span 
+                @click="firstPage()" 
+                class="w-16 p-2 me-4 rounded-lg text-center cursor-pointer" 
+                :class="{
+                    'bg-amber-600': meta.current_page != 1,
+                    'hover:bg-amber-800': meta.current_page != 1,
+                    'bg-neutral-600': meta.current_page == 1,
+                    'cursor-pointer': meta.current_page != 1,
+                }"
+                >
+                first
+            </span>
+            <span 
+                @click="previusPage()" 
+                class="w-16 p-2 me-4 rounded-lg text-center"
+                :class="{
+                    'bg-amber-600': meta.current_page != 1,
+                    'hover:bg-amber-800': meta.current_page != 1,
+                    'bg-neutral-600': meta.current_page == 1,
+                    'cursor-pointer': meta.current_page != 1,
+                }"
+                >
+                previus
+            </span>
+            <span 
+                @click="nextPage()" 
+                class="w-16 p-2 me-4 rounded-lg text-center"
+                :class="{
+                    'bg-amber-600': meta.current_page != meta.last_page,
+                    'hover:bg-amber-800': meta.current_page != meta.last_page,
+                    'bg-neutral-600': meta.current_page == meta.last_page,
+                    'cursor-pointer': meta.current_page != meta.last_page,
+                }"
+                >
+                next
+            </span>
+            <span 
+                @click="lastPage()" 
+                class="w-16 p-2 rounded-lg text-center"
+                :class="{
+                    'bg-amber-600': meta.current_page != meta.last_page,
+                    'hover:bg-amber-800': meta.current_page != meta.last_page,
+                    'bg-neutral-600': meta.current_page == meta.last_page,
+                    'cursor-pointer': meta.current_page != meta.last_page,
+                }"
+                >
+                last
+            </span>
         </div>
     </div>
 </template>
