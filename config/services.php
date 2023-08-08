b44873f40ca94d71526f77b9480481c9b6c78879
Furs Andrei <you@example.com>
2023-08-08 19:34:39 +0300

    debug send message

--- config/services.php
+++ config/services.php
@@ -30,10 +30,10 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
-    
-    'telegram'=>[
-		'admin_id'=>env('TELEGRAM_ADMIN_ID'),
-		'administartion_group_id'=>env('TELEGRAM_ADMINISTRATION_GROUP_ID'),
-		'bot_token'=>env('TELEGRAM_TOKEN_BOT'),
+
+    'telegram' => [
+		'admin_id' => env('TELEGRAM_ADMIN_ID'),
+		'administartion_group_id' => env('TELEGRAM_ADMINISTRATION_GROUP_ID'),
+		'bot_token' => env('TELEGRAM_TOKEN_BOT'),
     ],
 ];
