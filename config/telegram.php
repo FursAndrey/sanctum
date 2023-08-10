5e7dc88209d64b9b5095b333a6ff19eb40c0b87e
Furs Andrei <you@example.com>
2023-08-10 19:53:15 +0300

    change gitignore file

--- config/telegram.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'admin_id' => env('TELEGRAM_ADMIN_ID', ''),
-    'administartion_group_id' => env('TELEGRAM_ADMINISTRATION_GROUP_ID', ''),
-    'bot_token' => env('TELEGRAM_TOKEN_BOT', ''),
-];
