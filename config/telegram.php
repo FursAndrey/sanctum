f077cf14aa36e991c6872a78444be3e107f05446
Furs Andrei <you@example.com>
2023-08-10 19:25:05 +0300

    change telegram config

--- config/telegram.php
+++ config/telegram.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'admin_id' => env('TELEGRAM_ADMIN_ID'),
-    'administartion_group_id' => env('TELEGRAM_ADMINISTRATION_GROUP_ID'),
-    'bot_token' => env('TELEGRAM_TOKEN_BOT'),
+    'admin_id' => env('TELEGRAM_ADMIN_ID', ''),
+    'administartion_group_id' => env('TELEGRAM_ADMINISTRATION_GROUP_ID', ''),
+    'bot_token' => env('TELEGRAM_TOKEN_BOT', ''),
 ];
