8ff1d4252083765668370f75d9f60e90b011a979
Furs Andrei <you@example.com>
2023-08-20 11:01:15 +0300

    tg use tg name instead of admin id

--- app/Console/Commands/MyDebug.php
+++ app/Console/Commands/MyDebug.php
@@ -2,6 +2,8 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Role;
+use App\Models\User;
 use App\Services\TelegramService;
 use Illuminate\Console\Command;
 
@@ -28,15 +30,21 @@ class MyDebug extends Command
     {
         echo "Hello World!";
         
-        $message = 'Доступ разрешен.';
-        $keyboard = [
-            [
-                ['text' => 'Добавить 1 случайный пост', 'callback_data' => 'create-random-post'],
-            ],
-        ];
+        $q = User::with(['roles'])->whereRelation('roles', 'title', '=', 'Admin')->get('tg_name')->map->tg_name->toArray();
+
+        dump($q);
+
+        dump(in_array('test', $q));
+        dump(in_array('test3', $q));
+        // $message = 'Доступ разрешен.';
+        // $keyboard = [
+        //     [
+        //         ['text' => 'Добавить 1 случайный пост', 'callback_data' => 'create-random-post'],
+        //     ],
+        // ];
         
-        $admin_id = config('telegram.admin_id');
-        $telegramService = new TelegramService();
-        $telegramService->sendMessage($admin_id, $message, $keyboard);
+        // $admin_id = config('telegram.admin_id');
+        // $telegramService = new TelegramService();
+        // $telegramService->sendMessage($admin_id, $message, $keyboard);
     }
 }
