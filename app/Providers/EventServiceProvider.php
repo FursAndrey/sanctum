932b9c12e6daaeb0cffbdc7f5a875bf1aa83ec14
Furs Andrei <you@example.com>
2023-08-24 18:48:42 +0300

    add comment observer

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Comment;
 use App\Models\Post;
+use App\Observers\CommentObserver;
 use App\Observers\PostObserver;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
@@ -28,6 +30,7 @@ class EventServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Post::observe(new PostObserver());
+        Comment::observe(new CommentObserver());
     }
 
     /**
