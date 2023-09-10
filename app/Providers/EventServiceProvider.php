2aa029d00077b2e56fc30d5b83fa4c8d6ed2b908
Furs Andrei <you@example.com>
2023-09-10 16:42:18 +0300

    disable observers for testing

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -29,8 +29,10 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Post::observe(new PostObserver());
-        Comment::observe(new CommentObserver());
+        if (config('app.env') !== 'testing') {
+            Post::observe(new PostObserver());
+            Comment::observe(new CommentObserver());
+        }
     }
 
     /**
