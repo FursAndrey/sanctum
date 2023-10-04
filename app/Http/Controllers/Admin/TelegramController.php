2dad19f1a95975a43da75815fcae575adcb9eca0
Furs Andrei <you@example.com>
2023-10-04 20:04:19 +0300

    refactoring telegram controller

--- app/Http/Controllers/Admin/TelegramController.php
+++ app/Http/Controllers/Admin/TelegramController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers\Admin;
 
 use App\Actions\Comment\createRandomCommentAction;
 use App\Actions\Post\createPostWithPreviewAction;
+use App\Actions\Telegram\removeMenuAndItsCallActions;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Services\TelegramService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\Rule;
 
 class TelegramController extends Controller
 {
@@ -20,123 +22,100 @@ class TelegramController extends Controller
         // Log::info($event);
 
         if (
-            isset($event['message'])
-            && is_array($event['message'])
-            && isset($event['message']['chat'])
-            && is_array($event['message']['chat'])
-            && isset($event['message']['chat']['id'])
-            && is_int($event['message']['chat']['id'])
-
-            && isset($event['message']['chat']['username'])
-            && is_string($event['message']['chat']['username'])
-            && in_array($event['message']['chat']['username'], $adminTgNames)
-            && isset($event['message']['text'])
-            && $event['message']['text'] == 'admin-control'
-            && isset($event['message']['message_id'])
-            && is_int($event['message']['message_id'])
+            isset($event['callback_query'])
+            && is_array($event['callback_query'])
         ) {
-            $message_id = $event['message']['message_id'].'-';
-            //отправить кнопки управления
-            $message = 'Доступ разрешен.';
-            $keyboard = [
-                [
-                    ['text' => 'Добавить 1 случайный пост', 'callback_data' => $message_id.'create-random-post'],
-                    ['text' => 'Добавить 1 случайный комментарий', 'callback_data' => $message_id.'create-random-comment'],
+            $this->validate($request, [
+                'callback_query' => [
+                    'required',
+                    'array',
                 ],
-                [
-                    ['text' => 'Отмена', 'callback_data' => $message_id.'cancel'],
+                'callback_query.message' => [
+                    'required',
+                    'array',
                 ],
-            ];
-            $admin_id = $event['message']['chat']['id'];
+                'callback_query.message.message_id' => [
+                    'required',
+                    'integer',
+                ],
+                'callback_query.message.chat' => [
+                    'required',
+                    'array',
+                ],
+                'callback_query.message.chat.id' => [
+                    'required',
+                    'integer',
+                ],
+                'callback_query.message.chat.username' => [
+                    'required',
+                    'string',
+                    Rule::in($adminTgNames),
+                ],
+                'callback_query.data' => [
+                    'required',
+                    'string',
+                ],
+            ]);
 
-            $telegramService = new TelegramService();
-            $telegramService->sendMessage($admin_id, $message, $keyboard);
-        } elseif (
-            isset($event['callback_query'])
-            && is_array($event['callback_query'])
-            && isset($event['callback_query']['message'])
-            && is_array($event['callback_query']['message'])
-            && isset($event['callback_query']['message']['chat'])
-            && is_array($event['callback_query']['message']['chat'])
-            && isset($event['callback_query']['message']['chat']['id'])
-            && is_int($event['callback_query']['message']['chat']['id'])
-            && isset($event['callback_query']['message']['message_id'])
-            && is_int($event['callback_query']['message']['message_id'])
+            if (strripos($event['callback_query']['data'], 'create-random-post') !== false) {
+                //создать рандомный пост
+                (new createPostWithPreviewAction())();
+                (new removeMenuAndItsCallActions())($event);
+            } elseif (strripos($event['callback_query']['data'], 'create-random-comment') !== false) {
+                //создать рандомный комментарий
+                (new createRandomCommentAction())();
+                (new removeMenuAndItsCallActions())($event);
+            } elseif (strripos($event['callback_query']['data'], 'cancel') !== false) {
+                //выйти из меню (без изменений)
+                (new removeMenuAndItsCallActions())($event);
+            }
+        } else {
+            $this->validate($request, [
+                'message' => [
+                    'required',
+                    'array',
+                ],
+                'message.message_id' => [
+                    'required',
+                    'integer',
+                ],
+                'message.chat' => [
+                    'required',
+                    'array',
+                ],
+                'message.chat.id' => [
+                    'required',
+                    'integer',
+                ],
+                'message.chat.username' => [
+                    'required',
+                    'string',
+                    Rule::in($adminTgNames),
+                ],
+                'message.text' => [
+                    'required',
+                    'string',
+                ],
+            ]);
 
-            && isset($event['callback_query']['message']['chat']['username'])
-            && is_string($event['callback_query']['message']['chat']['username'])
-            && in_array($event['callback_query']['message']['chat']['username'], $adminTgNames)
-            && isset($event['callback_query']['data'])
-            && strripos($event['callback_query']['data'], 'create-random-post') !== false
-        ) {
-            //создать рандомный пост
-            (new createPostWithPreviewAction())();
-            $message_id = $event['callback_query']['message']['message_id'];
+            if ($event['message']['text'] == 'admin-control') {
+                $message_id = $event['message']['message_id'].'-';
+                //отправить кнопки управления
+                $keyboard = [
+                    [
+                        ['text' => 'Добавить 1 случайный пост', 'callback_data' => $message_id.'create-random-post'],
+                        ['text' => 'Добавить 1 случайный комментарий', 'callback_data' => $message_id.'create-random-comment'],
+                    ],
+                    [
+                        ['text' => 'Отмена', 'callback_data' => $message_id.'cancel'],
+                    ],
+                ];
+                $message = 'Доступ разрешен.';
+                $admin_id = $event['message']['chat']['id'];
 
-            $callback_data = explode('-', $event['callback_query']['data']);
-            $id = $callback_data[0];
-            $admin_id = $event['callback_query']['message']['chat']['id'];
-
-            $telegramService = new TelegramService();
-            $telegramService->deleteMessage($admin_id, $message_id);
-            $telegramService->deleteMessage($admin_id, $id);
-        } elseif (
-            isset($event['callback_query'])
-            && is_array($event['callback_query'])
-            && isset($event['callback_query']['message'])
-            && is_array($event['callback_query']['message'])
-            && isset($event['callback_query']['message']['chat'])
-            && is_array($event['callback_query']['message']['chat'])
-            && isset($event['callback_query']['message']['chat']['id'])
-            && is_int($event['callback_query']['message']['chat']['id'])
-            && isset($event['callback_query']['message']['message_id'])
-            && is_int($event['callback_query']['message']['message_id'])
-
-            && isset($event['callback_query']['message']['chat']['username'])
-            && is_string($event['callback_query']['message']['chat']['username'])
-            && in_array($event['callback_query']['message']['chat']['username'], $adminTgNames)
-            && isset($event['callback_query']['data'])
-            && strripos($event['callback_query']['data'], 'create-random-comment') !== false
-        ) {
-            //создать рандомный комментарий
-            (new createRandomCommentAction())();
-            $message_id = $event['callback_query']['message']['message_id'];
-
-            $callback_data = explode('-', $event['callback_query']['data']);
-            $id = $callback_data[0];
-            $admin_id = $event['callback_query']['message']['chat']['id'];
-
-            $telegramService = new TelegramService();
-            $telegramService->deleteMessage($admin_id, $message_id);
-            $telegramService->deleteMessage($admin_id, $id);
-        } elseif (
-            isset($event['callback_query'])
-            && is_array($event['callback_query'])
-            && isset($event['callback_query']['message'])
-            && is_array($event['callback_query']['message'])
-            && isset($event['callback_query']['message']['chat'])
-            && is_array($event['callback_query']['message']['chat'])
-            && isset($event['callback_query']['message']['chat']['id'])
-            && is_int($event['callback_query']['message']['chat']['id'])
-            && isset($event['callback_query']['message']['message_id'])
-            && is_int($event['callback_query']['message']['message_id'])
-
-            && isset($event['callback_query']['message']['chat']['username'])
-            && is_string($event['callback_query']['message']['chat']['username'])
-            && in_array($event['callback_query']['message']['chat']['username'], $adminTgNames)
-            && isset($event['callback_query']['data'])
-            && strripos($event['callback_query']['data'], 'cancel') !== false
-        ) {
-            //выйти из меню (без изменений)
-            $message_id = $event['callback_query']['message']['message_id'];
-
-            $callback_data = explode('-', $event['callback_query']['data']);
-            $id = $callback_data[0];
-            $admin_id = $event['callback_query']['message']['chat']['id'];
-
-            $telegramService = new TelegramService();
-            $telegramService->deleteMessage($admin_id, $message_id);
-            $telegramService->deleteMessage($admin_id, $id);
+                $telegramService = new TelegramService();
+                $telegramService->sendMessage($admin_id, $message, $keyboard);
+            }
         }
     }
 }
