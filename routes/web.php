5f3219683c135f126151e95ff69a545374b5d789
Furs Andrei <you@example.com>
2024-04-26 19:17:41 +0300

    add swagger

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,3 @@ Auth::routes();
 Route::get('/{any}', function () {
     return view('welcome');
 })->where('any', '.*');
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
